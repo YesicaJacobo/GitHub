c5f90961fb79fb077bb8be37adede184282c2e1e
yesicajv <yjv15.yjv@gmail.com>
2021-08-26 17:08:01 -0700

    prueban2

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> GitHub </title>
+    </head>
+    <body bgcolor="#FFAFBC">
+        
+        <font color="#610313">
+            <center>
+                <h1> Bienvenido a mi servidor personal de GitHub </h1>
+                <h2>Materia: Practicas de Desarollo de Sistemas II</h2>
+                <h2>Alumna: Yesica Irasema Jacobo Viruel</h2>
+            
+                <p>Hola bienvenid@ a mi pagina web en GitHub, soy estudiante en ingenieria en sistemas, me encanta el amarillo y bailar c:</p>
+            
+            
+                <br>
+                <img src = "./img/caballo.png" alt ="Bello caballo">
+                <img src = "./img/mariposa.png" alt ="Linda mariposa">
+            </center>
+        
+            <br><a href="https://yesicajacobo.repl.co/">   mira mi pagina en replit</a>
+            <br><a href="https://yesicajacobo.000webhostapp.com/">   mira mi pagina en 000webhost </a>
+            <br><a href="https://yesicajacobo.herokuapp.com/">   mira mi pagina en Heroku </a>
+        
+            <center>
+                <h3>Byeee \(^-^)</h3>
+            </center>
+        
+            <br><br><br><p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae modi qui maxime! Itaque, ex quisquam id quaerat dolore maxime voluptates, placeat reiciendis vitae doloremque, atque ipsum. Consectetur doloribus dolorem facere.
+            </p>
+        </font>
+        
+    </body>
+</html>
